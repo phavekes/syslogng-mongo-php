6abb728f17082520ef276ff29b7ed567096af45d
Peter Havekes <peter@havekes.eu>
2014-09-26 08:53:24 +0200

    code cleanup

--- json-data.php
+++ json-data.php
@@ -1,26 +1,33 @@
 <?php
+/*
+    This file is calles by jquery to get results from the database and display the table
+*/
 header('Content-type: application/json');
 // Configuration
 $dbhost = 'localhost';
 $dbname = 'syslog';
+// end configuration
 
-// Connect to test database
+
+// Connect to the database
 $m = new Mongo("mongodb://$dbhost");
 $db = $m->$dbname;
 
-
-//db.messages.find({"MESSAGE":/^dhcp/i})
+//Get the query parameters
+//first set the defaults
 $message=".";
 $program=".";
 $host=".";
 $prio=".";
 $limit=100;
+//Fetch the parameters, if set
 if ((isset($_GET['host']))&&(strlen($_GET['host'])>2)) $host=$_GET['host'];
 if ((isset($_GET['message']))&&(strlen($_GET['message'])>2)) $message=$_GET['message'];
 if ((isset($_GET['limit']))&&(strlen($_GET['limit'])>2)) $limit=$_GET['limit'];
 if ((isset($_GET['prio']))&&(strlen($_GET['prio'])>2)) $prio=$_GET['prio'];
 if ((isset($_GET['program']))&&(strlen($_GET['program'])>2)) $program=$_GET['program'];
 
+//build the mongo query
 $where = (
             array('$and' => array(
                 array('MESSAGE' => array('$regex' => new MongoRegex("/$message/i"))),
@@ -31,10 +38,12 @@ $where = (
             )
         );
 try {
+    //execute the query, and return the json
     $logs = $db->messages->find($where)->timeout(240000)->limit($limit)->sort(array("DATE" => -1));
     echo json_encode(iterator_to_array($logs));
 
 } catch (Exception $e) {
     echo 'Caught exception: ',  $e->getMessage(), "\n";
 }
+
 ?>
